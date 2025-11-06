98d0fdc8c2cf24f7c60f8df464cda727f0dede22
dejayillegal <119801523+dejayillegal@users.noreply.github.com>
2025-11-06 22:03:50 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <Router base="/jayakumarm">
+      <App />
+    </Router>
+  </React.StrictMode>
+);
