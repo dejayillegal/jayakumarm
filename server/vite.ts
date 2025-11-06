dec60acc8fef0332f64b17f12171193338baaa3a
dejayillegal <119801523+dejayillegal@users.noreply.github.com>
2025-11-06 22:05:06 +0530

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -5,8 +5,11 @@ import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
+import { fileURLToPath } from "url";
 
-const viteLogger = createLogger();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
